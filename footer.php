d999b12f3a34f4580b8febc04d18d93857cde598
Poonam kanuajiya <69005059+Poonam798@users.noreply.github.com>
2021-01-14 12:01:11 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<link rel='stylesheet' type='text/css' media='screen' href='footer.css'>
+<center>
+    <div class="footer">
+        <span>
+            &copy; <?php echo date("Y")?> The Spark Foundation,Basic Banking System || <label class="name">Developed by Poonam</label> 
+        </span>
+    </div>
+</center>
+<style>
+
+</style>
